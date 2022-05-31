d65ae6f2243da3e38665b7e67084f91731c6aeb4
cycle <cai1113@cycle.com>
2022-05-31 12:01:50 +0800

    add tableapi

--- src/views/dataManagement/business/index.vue
+++ src/views/dataManagement/business/index.vue
@@ -1,9 +1,138 @@
 <template>
-  <div>测试</div>
+  <div class="table-container">
+    <el-form
+      :inline="true"
+      class="form-inline"
+    >
+      <el-form-item label="审批人">
+        <el-input
+          placeholder="审批人"
+        />
+      </el-form-item>
+      <el-form-item label="活动区域">
+        <el-select
+          placeholder="活动区域"
+        >
+          <el-option
+            label="区域一"
+            value="shanghai"
+          />
+          <el-option
+            label="区域二"
+            value="beijing"
+          />
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button
+          type="primary"
+        >
+          查询
+        </el-button>
+      </el-form-item>
+    </el-form>
+    <el-table
+      :data="state.tableData"
+      style="width: 100%"
+    >
+      <el-table-column
+        prop="date"
+        label="Date"
+        width="180"
+      />
+      <el-table-column
+        prop="name"
+        label="Name"
+        width="180"
+      />
+      <el-table-column
+        prop="address"
+        label="Address"
+      />
+      <el-table-column
+        prop="tag"
+        label="Tag"
+      />
+    </el-table>
+  </div>
 </template>
 
 <script lang="ts" setup>
-
+import { reactive } from 'vue'
+const state = reactive({
+  tableData: [
+    {
+      date: '2016-05-07',
+      name: '白小白',
+      address: '上海市普陀区金沙江路 1518 弄',
+      tag: '家'
+    },
+    {
+      date: '2016-05-02',
+      name: '王小虎',
+      address: '上海市普陀区金沙江路 1518 弄',
+      tag: '家'
+    },
+    {
+      date: '2016-05-04',
+      name: '李小胖',
+      address: '上海市普陀区金沙江路 1517 弄',
+      tag: '公司'
+    },
+    {
+      date: '2016-05-01',
+      name: '王老五',
+      address: '上海市普陀区金沙江路 1519 弄',
+      tag: '家'
+    },
+    {
+      date: '2016-07-03',
+      name: '王麻子',
+      address: '上海市普陀区金沙江路 1516 弄',
+      tag: '公司'
+    },
+    {
+      date: '2016-07-07',
+      name: '白小白',
+      address: '上海市普陀区金沙江路 1518 弄',
+      tag: '家'
+    },
+    {
+      date: '2016-07-02',
+      name: '王小虎',
+      address: '上海市普陀区金沙江路 1518 弄',
+      tag: '家'
+    },
+    {
+      date: '2016-07-04',
+      name: '李小胖',
+      address: '上海市普陀区金沙江路 1517 弄',
+      tag: '公司'
+    },
+    {
+      date: '2016-07-01',
+      name: '王老五',
+      address: '上海市普陀区金沙江路 1519 弄',
+      tag: '家'
+    }
+  ],
+  currentPage: 1,
+  pageSize: 5,
+  search: ''
+})
 </script>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.el-main{
+  line-height: 20px;
+}
+.table-container{
+    .form-inline{
+        margin:15px;
+        text-align:left;
+    }
+    .table-list{
+        margin:15px;
+    }
+
+}</style>
